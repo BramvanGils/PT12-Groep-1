07f3bfdc31c520b2fed893b3febdc7552660908e
BramvanGils <38420547+BramvanGils@users.noreply.github.com>
2018-05-28 09:27:50 +0200

    Software Project Aangemaakt

---
+++ NeverBoardSoftwareApplicatie/NeverBoardSoftwareApplicatie/Background.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeverBoardSoftwareApplicatie
+{
+    public partial class Background : Form
+    {
+        public Background()
+        {
+            
+            InitializeComponent();
+            
+            Show();
+            Hide();
+            Show();
+
+            BackColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Opacity = 100;
+            Graphics gfx = CreateGraphics();
+            Pen p = new Pen(Color.Red);
+            gfx.DrawRectangle(p,new Rectangle(new Point(0,0),new Size(1919,1079)));
+        }
+    }
+}
